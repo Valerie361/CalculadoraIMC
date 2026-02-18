7b4b8ac6914c3e73963317d3912f900d759a01ce
Valerie <valeriepabon361@gmail.com>
2026-02-18 18:57:16 -0500

    Segundo Cambio

--- javascript.js
+++ javascript.js
@@ -12,36 +12,46 @@ function calcularIMC() {
         document.getElementById("valorIMC").style.color = "green";
     } 
     else {
-        document.getElementById("valorIMC").style.color = "red";
+        document.getElementById("valorIMC").style.color = "red"; 
     }
-    valorIMC.innerText = imc.toFixed(1)
-
+    // nunca pedir un valor ANTES del resultado, porque si el usuario no ingresa un valor, 
+    // el resultado se mostrará como "NaN" y eso no es lo que queremos mostrar al usuario. Por eso, 
+    // primero verificamos si los valores ingresados son válidos antes de mostrar el resultado del IMC.
+    
     if (isNaN(peso.value) || isNaN(altura.value) || peso.value <= 0 || altura.value <= 0) {
         resultado.innerText = "Por favor, ingrese todos los campos correctamente 😊";
         return;
     }
     else if (imc <= 16) {
+        valorIMC.innerText = imc.toFixed(1)
         resultado.innerText = "Desnutrición severa";
     }
     else if (imc <= 18.4) {
+        valorIMC.innerText = imc.toFixed(1)
         resultado.innerText = "Desnutricion moderada";
     }
     else if (imc <= 22) {
+        valorIMC.innerText = imc.toFixed(1)
         resultado.innerText = "Bajo peso";
     }
     else if (imc <= 24.9) {
+        valorIMC.innerText = imc.toFixed(1)
         resultado.innerText = "Peso normal";
     }
     else if (imc <= 29.9) {
+        valorIMC.innerText = imc.toFixed(1)
         resultado.innerText = "Sobrepeso";
     }
     else if (imc <= 34.9) {
+        valorIMC.innerText = imc.toFixed(1)
         resultado.innerText = "Obesidad tipo 1";
     }
     else if (imc <= 39.9) {
+        valorIMC.innerText = imc.toFixed(1)
         resultado.innerText = "Obesidad tipo 2";
     }
     else {
+        valorIMC.innerText = imc.toFixed(1)
         resultado.innerText = "Obesidad tipo 3";
     }
     
